d111a1fd3d788ddfd7d2d5f1644504dc777cc9fa
nitroman1991 <nitroman@list.ru>
2015-03-12 13:50:22 +0400

    no matlab

--- gen-report.sh
+++ gen-report.sh
@@ -1,19 +1,18 @@
 ﻿#!/bin/bash
 
 d=$1
-mattype=$2
 suffix=$(date '+%y%m%d-%H%M%S')
-if [ -z "$3" ]
+if [ -z "$2" ]
   then
     M=10
   else
-  	M=$3
+  	M=$2
 fi
-if [ -z "$4" ]
+if [ -z "$3" ]
   then
     N=10
   else
-  	N=$4
+  	N=$3
 fi
 python python/visualize.py -x data/$d -y $mattype -e data/$d.val.csv -v data/$d.vec.csv -c data/$d.coords.csv -N $N -M $M -t $d
 cd reports/latex && pdflatex tmp >/dev/null && pdflatex tmp >/dev/null && mv tmp.pdf ../$d-$suffix.pdf && cd ../..
