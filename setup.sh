140ee6db262be2e7bfcf20f9e88f7246ac3885a0
nitroman1991 <nitroman@list.ru>
2015-03-12 15:27:55 +0400

    -y option

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo -e "Starting installation of required c++ libraries...\n\n"
-sudo apt-get install libarpack2-dev libsuperlu3-dev gfortran libatlas-dev libblas-dev libboost-system-dev libboost-date-time-dev libboost-filesystem-dev
+sudo apt-get -y install libarpack2-dev libsuperlu3-dev gfortran libatlas-dev libblas-dev libboost-system-dev libboost-date-time-dev libboost-filesystem-dev
 if [ $? -ne 0 ]; then
 	echo -e "Error with installing c++ libraries, aborting..."
 	return 1
 fi
 echo -e "Starting installation required gcc...\n\n"
-sudo apt-get install gcc g++ g++-4.7
+sudo apt-get -y install gcc g++ g++-4.7
 if [ $? -ne 0 ]; then
 	echo -e "Error with installing gcc, aborting..."
 	return 1
 fi
 echo -e "Starting installation required LaTex libraries(for report generating)...\n\n"
-sudo apt-get install texlive-latex-base texlive-latex-extra dvipng libfreetype6-dev libpng-dev
+sudo apt-get -y install texlive-latex-base texlive-latex-extra dvipng libfreetype6-dev libpng-dev
 if [ $? -ne 0 ]; then
 	echo -e "Error with installing LaTex libraries, aborting..."
 	return 1
 fi
 echo -e "Starting installation python...\n\n"
-sudo apt-get install python python-pip && sudo easy_install distribute & sudo apt-get install python-dev
+sudo apt-get -y install python python-pip && sudo easy_install distribute & sudo apt-get install python-dev
 if [ $? -ne 0 ]; then
 	echo -e "Error with installing c++ libraries, aborting..."
 	return 1
